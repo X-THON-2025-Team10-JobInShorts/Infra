b0cd215133c776660792f732bdbabf8fe59950e2
Choi Yunho <cyunho62100@dgu.ac.kr>
2025-11-22 15:41:15 +0900

    [FEAT] Implement S3

---
+++ Terraform/s3/s3_bucket.tf
@@ -0,0 +1,28 @@
+resource "aws_s3_bucket" "tfer--x-thon-video-bucket" {
+  bucket        = var.bucket_name
+  force_destroy = "false"
+
+  grant {
+    id          = var.canonical_user_id
+    permissions = ["FULL_CONTROL"]
+    type        = "CanonicalUser"
+  }
+
+  object_lock_enabled = "false"
+  request_payer       = "BucketOwner"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+
+      bucket_key_enabled = "true"
+    }
+  }
+
+  versioning {
+    enabled    = "false"
+    mfa_delete = "false"
+  }
+}
---
+++ Terraform/s3/variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
+
+variable "canonical_user_id" {
+  description = "Canonical User ID for S3 grant"
+  type        = string
+}
