15a2c4ad4ccfdcb02a227a23bbdced9d90f13811
Choi Yunho <cyunho62100@dgu.ac.kr>
2025-11-22 15:40:47 +0900

    [FEAT] Implement Security Group

---
+++ Terraform/sg/outputs.tf
@@ -0,0 +1,11 @@
+output "aws_security_group_tfer--Kubernetes-sg_sg-067bf74f7fc603061_id" {
+  value = "${aws_security_group.tfer--Kubernetes-sg_sg-067bf74f7fc603061.id}"
+}
+
+output "aws_security_group_tfer--default_sg-0390471f6e0036f79_id" {
+  value = "${aws_security_group.tfer--default_sg-0390471f6e0036f79.id}"
+}
+
+output "aws_security_group_tfer--default_sg-06cc016c6b481d6fd_id" {
+  value = "${aws_security_group.tfer--default_sg-06cc016c6b481d6fd.id}"
+}
---
+++ Terraform/sg/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_id_1" {
+  description = "VPC ID for the first VPC"
+  type        = string
+}
+
+variable "vpc_id_2" {
+  description = "VPC ID for the second VPC"
+  type        = string
+}
